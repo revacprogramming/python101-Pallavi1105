56d7c23a7d8b562a1777f3e3d3d3b527b7164b5f
Pallavi1105 <94678026+Pallavi1105@users.noreply.github.com>
2022-05-16 07:20:32 +0000

    #03 fun

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,4 +1,11 @@
-hrs = float(input("Enter hours? "))
-rate=float(input("Enter Rate"))
-Earning=hrs*rate
-print("Pay",Earning)
+def Earning(hrs,rate):
+  wage=hrs*rate
+  return wage
+def output(wage):
+  print('Pay:',wage)
+def main():
+ hrs = float(input("Enter hours? "))
+ rate=float(input("Enter Rate"))
+ wage=Earning(hrs,rate)
+ output(wage)
+main()
