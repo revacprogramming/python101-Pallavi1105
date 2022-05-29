6e64ce95514f6da2ef6602d078b6e7a783f6f7dd
Pallavi1105 <94678026+Pallavi1105@users.noreply.github.com>
2022-05-29 17:46:33 +0000

    #11

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,18 @@
-# Tuples
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+dic = dict()
+for line in handle:
+    if "From:" in line:
+        continue
+    if "From" in line:
+        word_split = line.split()
+        hour = word_split[5].split(":")[0]
+        dic[hour] = dic.get(hour,0)+1
 
-filename = "dataset/mbox-short.txt"
+data =sorted([(v,k) for k,v in dic.items()],reverse=True)
+lst =list()
+for a,b in data:
+    lst.append([b,a])
+for a,b in sorted(lst):
+    print(a,b)
